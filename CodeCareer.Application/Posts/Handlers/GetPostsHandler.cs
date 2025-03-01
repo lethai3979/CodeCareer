4fac775fa1b2c7c21e85e179b2471536b3155d92
trongdat232003 <htdat233@gmail.com>
2025-03-01 14:19:52 +0700

    query post

---
+++ CodeCareer.Application/Posts/Handlers/GetPostsHandler.cs
@@ -0,0 +1,26 @@
+﻿using CodeCareer.Application.Posts.Queries;
+using CodeCareer.Application.UnitOfWork;
+using CodeCareer.Posts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCareer.Application.Posts.Handlers
+{
+    public class GetPostsHandler : IRequestHandler<GetPostQuery, IEnumerable<Post>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetPostsHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Post>> Handle(GetPostQuery request, CancellationToken cancellationToken)
+        {
+            return await _unitOfWork.PostRepository.GetAll();
+        }
+    }
+}
---
+++ CodeCareer.Application/Posts/Handlers/UpdatePostHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstraction.Commands;
+using CodeCareer.Application.Posts.Commands;
+using CodeCareer.Application.UnitOfWork;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCareer.Application.Posts.Handlers
+{
+    public class UpdatePostHandler: IRequestHandler<UpdatePostCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UpdatePostHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task Handle(UpdatePostCommand request, CancellationToken cancellationToken)
+        {
+            var 
+        }
+    }
+}
