435808a55e9867b6d28df7ba338784fb1e5bc351
lethai3979 <lethai3979@gmail.com>
2025-03-01 09:59:11 +0700

    add cqrs base interface

---
+++ CodeCareer.Application/Abstraction/Commands/ICommandHandler.cs
@@ -0,0 +1,20 @@
+﻿
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstraction.Commands
+{
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand>
+        where TCommand : ICommand 
+    {
+    }
+
+    public interface ICommandHandler<TCommand, TRespone> : IRequestHandler<TCommand, TRespone>
+        where TCommand : ICommand<TRespone>
+    {
+    }
+}
---
+++ CodeCareer.Application/Abstraction/Queries/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstraction.Queries
+{
+    public interface IQueryHandler<TQuery, TRespone> : IRequestHandler<TQuery, TRespone>
+        where TQuery : IQuery<TRespone>
+    {
+    }
+}
